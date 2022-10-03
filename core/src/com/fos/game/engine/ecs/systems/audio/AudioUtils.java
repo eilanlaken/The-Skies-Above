ada0365bc342d0d271fb9b9e34c3dc6bbf5ebc2a
Eilan <eilanlaken@gmail.com>
2022-10-03 13:13:57 -0500

    m

--- core/src/com/fos/game/engine/ecs/systems/audio/AudioUtils.java
+++ core/src/com/fos/game/engine/ecs/systems/audio/AudioUtils.java
@@ -1,21 +1,17 @@
 package com.fos.game.engine.ecs.systems.audio;
 
-import com.fos.game.engine.ecs.components.audio.ComponentSoundEffects;
-import com.fos.game.engine.ecs.components.base.ComponentType;
-import com.fos.game.engine.ecs.components.transform.ComponentTransform2D;
-import com.fos.game.engine.ecs.entities.Entity;
-
-import java.util.Comparator;
-
 public class AudioUtils {
 
+    /*
     private static final Comparator<ComponentSoundEffects> soundEffectsPriorityComparator = new Comparator<Entity>() {
         @Override
         public int compare(Entity e1, Entity e2) {
-            final float priority1 = ((ComponentTransform2D) e1.components[ComponentType.TRANSFORM_2D.ordinal()]).z;
-            final float priority2 = ((ComponentTransform2D) e2.components[ComponentType.TRANSFORM_2D.ordinal()]).z;
+            final float priority1 = ((ComponentSoundEffects) e1.components[ComponentType.AUDIO.ordinal()]).get(0).;
+            final float priority2 = ((ComponentSoundEffects) e2.components[ComponentType.AUDIO.ordinal()]).z;
             return priority1 > priority2 ? 1 : priority1 < priority2 ? -1 : 0;
         }
     };
+    
+     */
 
 }
