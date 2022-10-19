d2604b1b11af40e7f455aa8fee0c503af94b826b
Eilan <eilanlaken@gmail.com>
2022-10-18 20:10:32 -0500

    m

--- core/src/com/fos/game/engine/ecs/components/cameras/ComponentCamera2D.java
+++ core/src/com/fos/game/engine/ecs/components/cameras/ComponentCamera2D.java
@@ -1,8 +1,11 @@
 package com.fos.game.engine.ecs.components.cameras;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL30;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.glutils.FrameBuffer;
+import com.badlogic.gdx.graphics.glutils.GLFrameBuffer;
 import com.fos.game.engine.core.graphics.g2d.RenderTarget;
 import com.fos.game.engine.core.graphics.shaders.postprocessing.PostProcessingEffect;
 import com.fos.game.engine.ecs.components.base.Component;
@@ -25,6 +28,7 @@ public class ComponentCamera2D implements Component {
     public float pixelsPerMeterY;
     public PostProcessingEffect postProcessingEffect;
     public RenderTarget renderTarget;
+    public FrameBuffer frameBuffer;
 
     protected ComponentCamera2D(OrthographicCamera lens, final Enum[] layers, float depth, final RenderTarget.RenderTargetParams renderTargetParams, final PostProcessingEffect postProcessingEffect) {
         this.layers = new HashSet<>();
@@ -34,11 +38,21 @@ public class ComponentCamera2D implements Component {
         this.lens = lens;
         this.viewWorldWidth = lens.viewportWidth;
         this.viewWorldHeight = lens.viewportHeight;
-        System.out.println("view world width and height: " + this.viewWorldWidth + "," + this.viewWorldHeight);
         this.pixelsPerMeterX = (float) Gdx.graphics.getWidth() / lens.viewportWidth;
         this.pixelsPerMeterY = (float) Gdx.graphics.getHeight() / lens.viewportHeight;
         this.renderTarget = renderTargetParams == null ? null : new RenderTarget(renderTargetParams);
         this.postProcessingEffect = postProcessingEffect;
+        buildFrameBuffer();
+    }
+
+    public void buildFrameBuffer() {
+        final GLFrameBuffer.FrameBufferBuilder frameBufferBuilderScene = new GLFrameBuffer.FrameBufferBuilder(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        frameBufferBuilderScene.addColorTextureAttachment(GL30.GL_RGBA8, GL30.GL_RGBA, GL30.GL_UNSIGNED_BYTE);
+        frameBuffer = frameBufferBuilderScene.build();
+        for (Texture attached : frameBuffer.getTextureAttachments()) {
+            attached.setWrap(Texture.TextureWrap.ClampToEdge, Texture.TextureWrap.ClampToEdge);
+            attached.setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
+        }
     }
 
     public boolean isRenderToTexture() {
