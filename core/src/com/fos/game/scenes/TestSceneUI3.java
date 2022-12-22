a741e7483ba1fb329c010e36cb4665bd6b171239
Eilan <eilanlaken@gmail.com>
2022-12-21 22:03:23 -0600

    added typist into a vis-ui window

--- core/src/com/fos/game/scenes/TestSceneUI3.java
+++ core/src/com/fos/game/scenes/TestSceneUI3.java
@@ -9,7 +9,6 @@ import com.badlogic.gdx.utils.ScreenUtils;
 import com.fos.game.engine.core.context.ApplicationContext;
 import com.fos.game.engine.core.context.Scene;
 import com.fos.game.engine.core.graphics.ui.text.base.TypingLabel;
-import com.fos.game.engine.core.graphics.ui.text.effects.BlinkEffect;
 import com.kotcrab.vis.ui.VisUI;
 import com.kotcrab.vis.ui.widget.*;
 
@@ -44,41 +43,41 @@ public class TestSceneUI3 extends Scene {
             }
         } );
 
+        String text = "[GREEN]Hello,{WAIT} world!"
+                + "[ORANGE]{SLOWER} Did you know orange is my favorite color?";
+        TypingLabel label = new TypingLabel(text, VisUI.getSkin());
+
         //VisList<String> list = new VisList<>();
         List<String> list = new List<>(VisUI.getSkin());
-        list.setItems("hi", "bye", "mye");
+        list.setItems("hi", "bye", "mye","asd","sfdf", "hhh");
         list.pack();
         VisScrollPane visScrollPane = new VisScrollPane(list);
         VisTable table = new VisTable();
         list.setX(600);
         list.setY(600);
-        //table.add(visScrollPane);
-        //table.row();
-        //table.add(button);
-
-        BlinkEffect x;
 
         window = new VisWindow("window title");
         window.setX(300);
         window.setY(300);
         window.setWidth(300);
         window.setHeight(300);
+        window.setResizeBorder(22);
         window.addCloseButton();
+        window.debugAll();
         VisWindow.FADE_TIME = 0.01f;
         window.setResizable(true);
         window.add(textArea);
         window.row();
-        window.setColor(1,0,0,1);
-        //stage.addActor(textArea);
+        window.add(button);
+        window.row();
+        window.add(label);
+
+        //window.setColor(1,0,0,1);
         stage.addActor(window);
         stage.addActor(list);
 
-        String text = "[GREEN]Hello,{WAIT} world!"
-                + "[ORANGE]{SLOWER} Did you know orange is my favorite color?";
-        TypingLabel label = new TypingLabel(text, VisUI.getSkin());
-        label.setX(700);
-        label.setY(700);
-        stage.addActor(label);
+
+        //window.pack();
     }
 
     @Override
